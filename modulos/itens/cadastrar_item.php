7ba02089120903ff1f5921789061f215b34291d6
victormelimlauro <victormeliml@gmail.com  git config --global user.email victormeliml@gmail.com>
2023-09-10 16:07:08 -0300

    ajuste

--- modulos/itens/cadastrar_item.php
+++ modulos/itens/cadastrar_item.php
@@ -1,5 +1,10 @@
 <?php 
 
+session_start();
+
+if(!isset($_SESSION["usuario_logado"]))
+    header("Location: login.php");
+
 try {
 
     /**
--- modulos/itens/lista_itens.php
+++ modulos/itens/lista_itens.php
@@ -1,5 +1,11 @@
 <?php 
 
+session_start();
+
+if(!isset($_SESSION["usuario_logado"]))
+    header("Location: login.php");
+
+
 try {
 
     include "../../DAO/ItemDAO.php";
