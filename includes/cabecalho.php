2391f298f7228c41fe2516f77bc5a8f4269305e5
victormelimlauro <victormeliml@gmail.com  git config --global user.email victormeliml@gmail.com>
2023-09-09 22:39:34 -0300

    menu

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -6,6 +6,8 @@ if (!isset($_SESSION)) {
 try {
     $caminho = dirname(__FILE__) . '/../DAO/MySQL.php';
     include_once $caminho;
+    $caminho_index = dirname(__FILE__) . '/../index.php';
+   
     $mysql = new Mysql();
 
     $sql = "SELECT cod_usuario, nome, username FROM usuarios WHERE cod_usuario=:marcador_cod_usuario";
@@ -33,18 +35,18 @@ try {
             <li><a href="index.php">Tela inicial</a></li>
 
 
-            <li><a href="modulos/inventario/cadastrar_inventario.php">Criar Inventário</a></li>
-            <li><a href="modulos/inventario/lista_inventarios.php">Listar Inventários</a></li>
-            <li><a href="modulos/inventario/adicionar_item_inventario.php">Adicionar Item ao Inventário</a></li>
+            <li><a href="/modulos/inventario/cadastrar_inventario.php">Criar Inventário</a></li>
+            <li><a href="/modulos/inventario/lista_inventarios.php">Listar Inventários</a></li>
+            <li><a href="/modulos/inventario/adicionar_item_inventario.php">Adicionar Item ao Inventário</a></li>
 
-            <li><a href="modulos/local/cadastrar_local.php">Cadastrar Local</a></li>
-            <li><a href="modulos/local/listar_locais.php">Listar Locais</a></li>
+            <li><a href="/modulos/local/cadastrar_local.php">Cadastrar Local</a></li>
+            <li><a href="/modulos/local/listar_locais.php">Listar Locais</a></li>
 
-            <li><a href="#">Cadastrar tipo</a></li>
-            <li><a href="#">Listar Tipos</a></li>
+        <!--    <li><a href="#">Cadastrar tipo</a></li>
+            <li><a href="#">Listar Tipos</a></li> -->
 
-            <li><a href="modulos/itens/cadastrar_item.php">Cadastrar ativo</a></li>
-            <li><a href="modulos/itens/lista_itens.php">Listar ativos</a></li>
+            <li><a href="/modulos/itens/cadastrar_item.php">Cadastrar ativo</a></li>
+            <li><a href="/modulos/itens/lista_itens.php">Listar ativos</a></li>
         </ul>
     </nav>
     <hr>
