a91cd00c5241543c18a29e9672af8b7423ebabf4
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 02:14:24 -0300

    ok

--- modulos/operacoes_inventarios/listar_operacoes.php
+++ modulos/operacoes_inventarios/listar_operacoes.php
@@ -23,47 +23,70 @@ try {
 
 }
 ?>
-<html>
-    <head>
-        <title> Listar produtos </title>
-    </head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Listar Operações</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        main {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
 
-    <body>
-        <?php include '../../includes/cabecalho.php' ?>
+<?php include '../../includes/cabecalho.php' ?>
 
-        <main>
-        <table>
-                <thead>
-                    <tr>
-                        <th>Codigo operação</th>
-                        <th>Cod. Local:</th>
-                        <th>Nome local:</th>
-                        <th>Num. Pat. Item</th>
-                        <th>Nome Item</th>
-                        <th>Cod. Inventario</th>
-                        <th>Nome Inventario</th>
-                        <th>Cod. Usuário</th>
-                        <th>Nome Usuário</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php for($i=0; $i<$total_itens; $i++): ?>
-                    <tr>
-                        <td> <?= $lista_OperacaoInventario[$i]->cod_operacoes_inventarios ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->cod_local ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->nome_local ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->numpat_item ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->nome_item ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->cod_inventario ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->nome_inventario ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->cod_usuario ?> </td>
-                        <td> <?= $lista_OperacaoInventario[$i]->nome ?> </td>
-                        <td> 
-                            <a href="cadastrar_operacao.php?excluir=true&cod_operacoes_inventarios=<?= $lista_OperacaoInventario[$i]->cod_operacoes_inventarios ?>">
-                                Excluir</a> 
-                        </td>
-                    </tr>
-                    <?php endfor ?>
-                </tbody>
-            </table>
+<main class="container">
+    <h1 class="mb-4">Listar Operações</h1>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Código Operação</th>
+                    <th scope="col">Cod. Local</th>
+                    <th scope="col">Nome Local</th>
+                    <th scope="col">Num. Pat. Item</th>
+                    <th scope="col">Nome Item</th>
+                    <th scope="col">Cod. Inventário</th>
+                    <th scope="col">Nome Inventário</th>
+                    <th scope="col">Cod. Usuário</th>
+                    <th scope="col">Nome Usuário</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for($i=0; $i<$total_itens; $i++): ?>
+                <tr>
+                    <td><?= $lista_OperacaoInventario[$i]->cod_operacoes_inventarios ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->cod_local ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->nome_local ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->numpat_item ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->nome_item ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->cod_inventario ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->nome_inventario ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->cod_usuario ?></td>
+                    <td><?= $lista_OperacaoInventario[$i]->nome ?></td>
+                    <td>
+                        <a href="cadastrar_operacao.php?excluir=true&cod_operacoes_inventarios=<?= $lista_OperacaoInventario[$i]->cod_operacoes_inventarios ?>" class="btn btn-danger btn-sm">Excluir</a>
+                    </td>
+                </tr>
+                <?php endfor ?>
+            </tbody>
+        </table>
+    </div>
+</main>
+
+<?php include '../../includes/rodape.php' ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
