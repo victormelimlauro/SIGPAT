6094e040343b80b61957820cc784df38f90d006d
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 06:33:58 -0300

    RODAPE

--- includes/rodape.php
+++ includes/rodape.php
@@ -4,7 +4,7 @@
 <div class="container pt-3 pb-3">
 <div class="site-info">
 © 2023 <a href="https://sigpat.vmserv.com.br">SIGPAT</a> <span class="sep"> | </span>
-<a class="credits" href="https://vmserv.com.br/" target="_blank" title="Desenvolvedor do sistema" alt="Desenvolvido por vmserv.com.br">site by vmserv.com.br</a>
+<a class="credits" href="https://vmserv.com.br/" target="_blank" title="Desenvolvedor do sistema" alt="Desenvolvido por vmserv.com.br">Desenvolvido por VM SERV | Soluções em T.I. vmserv.com.br</a>
 </div>
 </div>
 </footer>
