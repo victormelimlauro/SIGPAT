8d1b1fd06933bb269cb911013449113cfca5021a
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 02:23:05 -0300

    menu

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -40,7 +40,7 @@ try {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="https://sigpat.vmserv.com.br/index.php">Inicio</a>
+            <a class="nav-link" aria-current="page" href="https://sigpat.vmserv.com.br/index.php">Inicio</a>
             </li>
             <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
