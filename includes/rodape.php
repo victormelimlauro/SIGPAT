40506391b03cc2ff8ad045270af71d1d4e3d5fa8
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 06:11:02 -0300

    FT

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,7 +1,11 @@
 <footer>
 
-    <div class="bottom">
-    <hr />  
-          <p> ©2023 SIGPAT - Sistema Integrado de Gestão do Patrimônio. </p>
-    </div>
+<footer id="colophon" class="site-footer navbar-light bg-light" role="contentinfo">
+<div class="container pt-3 pb-3">
+<div class="site-info">
+© 2023 <a href="https://sigpat.vmserv.com.br">SIGPAT</a> <span class="sep"> | </span>
+<a class="credits" href="https://vmserv.com.br/" target="_blank" title="WordPress Technical Support" alt="site by vmserv.com.br">site by vmserv.com.br</a>
+</div>
+</div>
+</footer>
 </footer>
