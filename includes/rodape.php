ca8f07986063c66cdf86796a6aa5478b4f4ee2e7
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 06:06:58 -0300

    rodape

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,5 +1,7 @@
 <footer>
-    <hr />
 
-    <p> ©2023 SIGPAT - Sistema Integrado de Gestão do Patrimônio. </p>
+    <div class="fixed-bottom">
+    <hr />  
+          <p> ©2023 SIGPAT - Sistema Integrado de Gestão do Patrimônio. </p>
+    </div>
 </footer>
