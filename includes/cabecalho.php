f63a94ee4a3f55f16a0196ffa478115c3c59be3a
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-19 00:52:27 -0300

    GIT TESTE

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -33,7 +33,7 @@ try {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">SIGPAT</a>
+        <a class="navbar-brand" href="#">SIGPAT GIT</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
