5da3336a04a60a841711a6192152936a54f2f890
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-20 10:14:33 -0300

    listainv

--- modulos/inventario/lista_inventarios.php
+++ modulos/inventario/lista_inventarios.php
@@ -11,24 +11,6 @@ try {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Listar Inventários</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        main {
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
-
 <?php include '../../includes/cabecalho.php' ?>
 
 <main class="container">
