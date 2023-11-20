57c07f33220a27011934a2c09ca66ebe3034ae3e
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-20 10:27:34 -0300

    LOCAIS

--- modulos/locais/listar_locais.php
+++ modulos/locais/listar_locais.php
@@ -21,23 +21,8 @@ try {
 
 }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Sistemas</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        main {
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
+
+
 
 <?php include '../../includes/cabecalho.php' ?>
 
@@ -71,5 +56,3 @@ try {
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
