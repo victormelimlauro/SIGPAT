9bb7d691b18133e801cb6fd24bf8a40ab5402ee1
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-20 12:12:00 -0300

    CADOP

--- modulos/operacoes_inventarios/cadastrar_operacao.php
+++ modulos/operacoes_inventarios/cadastrar_operacao.php
@@ -136,8 +136,6 @@ try {
 <head>
     <title>Cadastro de Operação de Inventário</title>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <?php include '../../includes/cabecalho.php' ?>
