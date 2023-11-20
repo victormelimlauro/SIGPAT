a827eb62ea648c8a2be1c0d2d88d1e40d25920a9
Victor Melim Lauro <victormeliml@gmail.com>
2023-11-20 10:23:36 -0300

    cadloc

--- modulos/locais/cadastrar_local.php
+++ modulos/locais/cadastrar_local.php
@@ -76,23 +76,8 @@ try {
 
 }
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Sistemas</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        main {
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
+
+
 
 <?php include '../../includes/cabecalho.php' ?>
 
@@ -121,5 +106,5 @@ try {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
+
 </html>
